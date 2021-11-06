0f915f7a416e3d0e00f51e7a7530bc496b02eae0
ganu07 <gborhade7@gmail.com>
2021-11-06 16:57:15 +0530

    final commit

--- config.py
+++ config.py
@@ -1,25 +1,28 @@
 from flask import *
-import mysql.connector
-
+# import mysql.connector
+import pymysql
 
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="data"
-)
-
-# mydb = mysql.connector.connect(
-#     host="us-cdbr-east-04.cleardb.com",
-#     user="b0da91e06ee504",
-#     password="6b0f2bdb",
-#     database="heroku_b521556c7f4725f"
+# mydb = pymysql.connect(
+#     host="localhost",
+#     user="root",
+#     password="root",
+#     database="data"
 # )
-cursor = mydb.cursor()
+#
+mydb = pymysql.connect(
+        host="us-cdbr-east-04.cleardb.com",
+        user="b0da91e06ee504",
+        password="6b0f2bdb",
+        database="heroku_b521556c7f4725f"
+    )
 
-
+a = 0
+while a == 0:
+    mydb.connect()
+    cursor = mydb.cursor()
+    a = 1
 
 if __name__ =='__main__':
     app.run(debug=True)
